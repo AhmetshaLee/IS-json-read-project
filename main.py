70d1f4c99054e2b168a747853dbe426c1866da58
Sharipov Akhmetsha <scharipov.arsen2013@yandex.ru>
2023-12-18 14:36:19 +0500

    Added new rows for data & change 'create json' event

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def display_edit_mode():
 
     window['-NAME-'].update(disabled=False)
     window['-SURNAME-'].update(disabled=False)
-    window['-BIRTHDATE-'].update(disabled=False)
+    window['-BIRTH_DATE-'].update(disabled=False)
 
 def display_save_mode():
     window['-ADD-'].update(disabled=True)
@@ -17,7 +17,7 @@ def display_save_mode():
 
     window['-NAME-'].update(disabled=False)
     window['-SURNAME-'].update(disabled=False)
-    window['-BIRTHDATE-'].update(disabled=False)
+    window['-BIRTH_DATE-'].update(disabled=False)
 
 menu_layout = [
     ['&Действие', ['Создать json', 'Выбрать json']]
@@ -32,35 +32,59 @@ toolbar_buttons = [[
 
 left_col = [
     [sg.Text('Список')],
-    [sg.Listbox(values=['Текст для проверки'], size=(35, 25), font=('None 12'), key='-LISTBOX-', enable_events=True)],
+    [sg.Listbox(values=['user_list'], size=(35, 25), font=('None 12'), key='-LISTBOX-', enable_events=True)],
     [sg.Button('Выбрать', key='-SELECT_BTN-')]
 ]
 
 right_col = [
-    [sg.Text('Имя'), sg.Push(), sg.InputText(size=(30,1), key='-NAME-', disabled=True)],
-    [sg.Text('Фамилия'), sg.Push(), sg.InputText(size=(30,1), key='-SURNAME-', disabled=True)],
-    [sg.Text('Дата рождения'), sg.Push(), sg.InputText(size=(30,1), key='-BIRTHDATE-', disabled=True)],
+    [sg.Text('Фамилия'), sg.Push(), sg.InputText(size=(30,1), key='-SURNAME-')],
+    [sg.Text('Имя'), sg.Push(), sg.InputText(size=(30,1), key='-NAME-')],
+    [sg.Text('Отчество'), sg.Push(), sg.InputText(size=(30,1), key='-PATRONYMIC-')],
+    [sg.Text('Дата рождения'), sg.Push(), sg.InputText(size=(30,1), key='-BIRTH_DATE-')],
+    [sg.Text('Место рождения'), sg.Push(), sg.InputText(size=(30,1), key='-BIRTH_PLACE-')],
+    [sg.Text('Место фактического проживания'), sg.Push(), sg.InputText(size=(30,1), key='-LIVE_PLACE-')],
+    [sg.Text('Пол'), sg.Push(), sg.Combo(['Мужской', 'Женский'], size=(28,1),key='-GENDER-')],
+    [sg.Text('Семейное положение'), sg.Push(), sg.Combo(['Женат', 'Замужем', '———'], size=(28,1),key='-MARITAL-')],
+    [sg.Text('Сведения о детях'), sg.Push(), sg.Combo(['Присутствуют', 'Нет'], size=(28,1),key='-CHILD_INFO-')],
+    [sg.Text('Паспорт (серия и номер)'), sg.Push(), sg.InputText(size=(30,1), key='-PASSPORT-')],
+    [sg.Text('ИНН'), sg.Push(), sg.InputText(size=(30,1), key='-INN-')],
+    [sg.Text('Номер телефона'), sg.Push(), sg.InputText(size=(30,1), key='-PHONE-')],
+    [sg.Text('E-mail (Почта)'), sg.Push(), sg.InputText(size=(30,1), key='-EMAIL-')],
 ]
 
 main_layout = [
     [sg.Menu(menu_layout,)],
     [sg.Frame('Инструменты', toolbar_buttons)],
     [sg.Column(left_col, element_justification='c'), sg.VSeparator(), sg.Column(right_col, vertical_alignment='t')],
-    [sg.StatusBar(f'file1, file2')]
+    [sg.StatusBar('filename', key='-STATUS_BAR-', auto_size_text=False)]
 ]
 
-window = sg.Window('работа с JSON', main_layout, resizable=False)
+window = sg.Window('работа с JSON', main_layout, resizable=False, finalize=True)
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
     elif event == 'Создать json':
-        default_file_name = 'users1.json'
-        with open(default_file_name, 'w', encoding='utf-8') as file:
-            users = {}
-            json.dump(users, file)
-        sg.popup(f'Файл с именем по умолчанию {default_file_name} создан!')
+        while True:
+            FILENAME = sg.popup_get_text('Введите название для файла', default_text='users')
+            if FILENAME is None:
+                print('closed')
+                break
+            elif FILENAME == '': # улучшить проверку на пробелы
+                sg.popup('Название файла не может быть пустым!')
+            else:
+                JSON_FILENAME = f'{FILENAME}.json'
+                print(JSON_FILENAME)
+                try:
+                    with open(JSON_FILENAME, 'x', encoding='utf-8') as file:
+                        users_data = {'users': []}
+                        json.dump(users_data, file, indent=4, ensure_ascii=False)
+                    sg.popup('created')
+                    break
+                except FileExistsError:
+                    sg.popup('Файл с таким именем уже существует!')
 
 
     elif event == 'Выбрать json':
@@ -105,78 +129,78 @@ while True:
 
 window.close()
 
-try:
-    with open('users.json', 'r', encoding='utf-8') as json_data:
-        users = json.load(json_data)
-except json.decoder.JSONDecodeError:
-    users = {}
-except (FileExistsError, OSError):
-    with open('users.json', 'w', encoding='utf-8') as file:
-        users = {}
-        json.dump(users, file)
+# try:
+#     with open('users.json', 'r', encoding='utf-8') as json_data:
+#         users = json.load(json_data)
+# except json.decoder.JSONDecodeError:
+#     users = {}
+# except (FileExistsError, OSError):
+#     with open('users.json', 'w', encoding='utf-8') as file:
+#         users = {}
+#         json.dump(users, file)
 
-while True:
-    print('Заполните следующие данные: ')
+# while True:
+    # print('Заполните следующие данные: ')
 
-    last_name = input('Фамилия: ')
-    first_name = input('Имя: ')
-    patronymic = input('Отчество: ')
+    # last_name = input('Фамилия: ')
+    # first_name = input('Имя: ')
+    # patronymic = input('Отчество: ')
 
-    birth_date = input('Дата рождения (ДД.ММ.ГГГГ): ')
-    birth_place = input('Место рождения (Город — Регион — Страна): ')
+    # birth_date = input('Дата рождения (ДД.ММ.ГГГГ): ')
+    # birth_place = input('Место рождения (Город — Регион — Страна): ')
 
-    live_place = input('Место фактического проживания (Регион — Город — Адрес): ')
+    # live_place = input('Место фактического проживания (Регион — Город — Адрес): ')
 
-    gender = input('Пол (М/Ж): ')
-    marital_status = input('Семейное положение (Женат/Замужем): ')
-    child_info = input('Сведения о детях (Есть/Нет): ')
+    # gender = input('Пол (М/Ж): ')
+    # marital_status = input('Семейное положение (Женат/Замужем): ')
+    # child_info = input('Сведения о детях (Есть/Нет): ')
 
-    passport = input('Паспортные данные (Серия и №): ')
-    inn_number = input('ИНН: ')
+    # passport = input('Паспортные данные (Серия и №): ')
+    # inn_number = input('ИНН: ')
 
-    phone = input('Номер телефона (Если несколько, то перечислить): ')
-    email = input('E-mail (Почта): ')
+    # phone = input('Номер телефона (Если несколько, то перечислить): ')
+    # email = input('E-mail (Почта): ')
 
     # city = input('Город: ')
     # address = input('Улица: ')
     # b_day, b_month, b_year = birth_date.split('.')
-    birth_city, birth_region, birth_country = birth_place.replace(',', ' ').split()
-    birth_place = ', '.join([birth_city, birth_region, birth_country])
+    # birth_city, birth_region, birth_country = birth_place.replace(',', ' ').split()
+    # birth_place = ', '.join([birth_city, birth_region, birth_country])
 
-    live_region, live_city, live_address = live_place.split(', ')
-    live_place = ', '.join([live_region, live_city, live_address])
+    # live_region, live_city, live_address = live_place.split(', ')
+    # live_place = ', '.join([live_region, live_city, live_address])
 
-    pass_series, pass_num = passport.replace(',', ' ').split()
-    passport = ' '.join([pass_series, pass_num])
+    # pass_series, pass_num = passport.replace(',', ' ').split()
+    # passport = ' '.join([pass_series, pass_num])
 
-    user = {
-        'last_name': last_name,
-        'first_name': first_name,
-        'patronymic': patronymic,
-        'birth_date': birth_date,
-        'gender': gender,
-        'marital_status': marital_status,
-        'child_info': child_info,
-        'passport_data': passport,
-        'INN': inn_number,
-        'birth_place': birth_place,
-        'address': live_place,
-        'phone': [phone],
-        'other_data': {
-            'e-mail': [email]
-        }
+    # user = {
+    #     'last_name': last_name,
+    #     'first_name': first_name,
+    #     'patronymic': patronymic,
+    #     'birth_date': birth_date,
+    #     'gender': gender,
+    #     'marital_status': marital_status,
+    #     'child_info': child_info,
+    #     'passport_data': passport,
+    #     'INN': inn_number,
+    #     'birth_place': birth_place,
+    #     'address': live_place,
+    #     'phone': [phone],
+    #     'other_data': {
+    #         'e-mail': [email]
+    #     }
 
-    }
+    # }
 
-    if 'users' not in users:
-        users['users'] = []
-    users['users'].append(user)
+#     if 'users' not in users:
+#         users['users'] = []
+#     users['users'].append(user)
 
-    users['count'] = len(users['users'])
+#     users['count'] = len(users['users'])
 
-    choice = input('Добавить ещё пользователя? (Да/Нет): ')
-    if choice.lower() in ['нет', 'no', 'n']:
-        break
+#     choice = input('Добавить ещё пользователя? (Да/Нет): ')
+#     if choice.lower() in ['нет', 'no', 'n']:
+#         break
 
-with open('users.json', 'r+', encoding='utf-8') as file:
-    json.dump(users, file, indent=4, ensure_ascii=False)
+# with open('users.json', 'r+', encoding='utf-8') as file:
+#     json.dump(users, file, indent=4, ensure_ascii=False)
